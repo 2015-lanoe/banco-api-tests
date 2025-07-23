c03883c5ff1a67b42cca5008ef0676d3fe888115
Ngfernandes <noeli.garcia.63@gmail.com>
2025-07-23 20:18:33 -0300

    adicionando a biblioteca .env para as variáveis locais

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai')
+require ( 'dotenv' ) . config ( )
 
 describe('tranferencias', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com status code 201 para tranferencias igual ou acima de 10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            console.log (process.env.BASE_URL)
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json') // como seto o cabeçalho para esta requisição
                 .send({
@@ -14,7 +16,7 @@ describe('tranferencias', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json') // como seto o cabeçalho para requisição
                 .set('Authorization', 'Bearer ' + token)
