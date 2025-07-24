716b5fb83ff73856c74acb56980c629e72690075
Ngfernandes <noeli.garcia.63@gmail.com>
2025-07-24 18:28:40 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/login.test.js
+++ test/login.test.js
@@ -1,17 +1,17 @@
 const request = require('supertest');
 const { expect} = require ('chai')
 require ( 'dotenv' ) . config ( )
+const postLogin = require('../fixtures/postLogin.json')
 
 describe('login' , () => {
     describe('POST /login', () => {
         it('Deve retornar 200 com um token em string quando usar credenciais válidas', async() => {
+            const bodyLogin = {... postLogin}
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type' , 'application/json') // como seto o cabeçalho para esta requisição
-                .send({
-                    "username": 'julio.lima',
-                    "senha": '123456'
-                })
+                .send(bodyLogin)
 
             
 
--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require ('../fixtures/postTransferencias.json')
 
 describe('transferencias', () => {
     describe('POST /transferencias', () => {
@@ -13,18 +14,13 @@ describe('transferencias', () => {
             }) 
 
         it('Deve retornar sucesso com status code 201 para tranferencias igual ou acima de 10,00', async () => {
-            
+            const bodyTransferencias = { ...postTransferencias }
 
             const resposta = await request(process.env.BASE_URL)
               .post('/transferencias')
               .set('Content-Type', 'application/json') // como seto o cabeçalho para requisição
               .set('Authorization', `Bearer ${token}`)
-              .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 11,
-                token: " "
-                })
+              .send (bodyTransferencias)
 
             expect(resposta.status).to.equal(201);
 
@@ -32,17 +28,14 @@ describe('transferencias', () => {
     
 
          it('Deve retornar falha com status code 422 para tranferencias abaixo de 10,00', async () => {
+             const bodyTransferencias = { ...postTransferencias }
+             bodyTransferencias.valor = 8
 
              const resposta = await request('http://localhost:3000')
               .post('/transferencias')
               .set('Content-Type', 'application/json') // como seto o cabeçalho para requisição
               .set('Authorization', `Bearer ${token}`)
-              .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 8,
-                token: " "
-            })
+              .send(bodyTransferencias)
 
         expect(resposta.status).to.equal(422);
     })
